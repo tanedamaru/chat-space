da5060fc4c411099389a20bd0752c21107ff91ac
Riku Taneda <riku.program@gmail.com>
2018-09-22 23:18:46 +0900

    complete group-function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,17 @@
 class MessagesController < ApplicationController
 
   def index
+    @current_user= current_user
+    @group = Group.find(params[:group_id])
   end
 
   def create
+  end
+
+  private
+
+  def message_params
+    params[:group_id]
   end
 
 end
