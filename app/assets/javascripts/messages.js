f6a0302df266076c7e99ad9813f512d9d2c1810c
Riku Taneda <riku.program@gmail.com>
2018-10-06 18:00:27 +0900

    asynchronous message

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,82 @@
+function buildHTML(message){
+    if (message.image.url && message.text) {
+      var html = `<div class="message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+        <div class="upper-message__date">
+          ${message.time}
+        </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.text}
+          </p>
+          <img class="lower-message__image" src="${message.image.url}">
+        </div>
+        </div>
+        `
+        return html;
+    } else if ( message.text ){
+      var html = `<div class="message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+        <div class="upper-message__date">
+          ${message.time}
+        </div>
+        </div>
+        <div class="lower-message">
+        <p class="lower-message__content">
+          ${message.text}
+        </p>
+        </div>
+        </div>`
+        return html;
+    } else if ( message.image.url ){
+      var html = `<div class="message">
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}
+          </div>
+        <div class="upper-message__date">
+          ${message.time}
+        </div>
+        </div>
+        <div class="lower-message">
+          <img class="lower-message__image" src="${message.image.url}">
+        </div>
+        </div>
+        `
+        return html;
+    }
+}
+
+$(function(){
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = location.href;
+    $.ajax({
+      url: href,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      $('#form__button').prop("disabled", false);
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.lower-message__content').val('');
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight},'fast');
+      $('#message_content').val('');
+    })
+    .fail(function(){
+      alert('通信に失敗しました');
+    });
+  });
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+  json.text  @message.content
+  json.image @message.image
+  json.time  @message.created_at
+  json.user_id  @message.user.id
+  json.user_name  @message.user.name
