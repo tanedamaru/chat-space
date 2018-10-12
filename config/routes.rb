3d9e0741e6572dd7ba5c2feb2ece3e0f57600381
Riku Taneda <riku.program@gmail.com>
2018-10-12 19:10:30 +0900

    re:setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root  'groups#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
-  end
-
-  resources :users, only: [:index] do
-    collection do
-      get 'index'
-    end
   end
 end
